0a3d98ac0b49557dd2dd4c6dc61f8e3ae014d88b
Andy Trofimov <interlark@gmail.com>
2022-09-22 21:55:40 +0500

    Add trailing slash for referer URL

--- ustvgo_iptv.py
+++ ustvgo_iptv.py
@@ -9,7 +9,7 @@ from tqdm.asyncio import tqdm
 
 USER_AGENT = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
               '(KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36')
-USTVGO_HEADERS = {'Referer': 'https://ustvgo.tv', 'User-Agent': USER_AGENT}
+USTVGO_HEADERS = {'Referer': 'https://ustvgo.tv/', 'User-Agent': USER_AGENT}
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s',
